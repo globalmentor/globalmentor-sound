22f67f1ea4556c4c8486700ea5c5cd192f5bd497
Garret Wilson <garret@globalmentor.com>
2014-08-03 16:04:15 +0000

    Reformatted all source code to more compact form with non-aligned braces.

--- trunk/globalmentor-sound/src/main/java/com/globalmentor/sound/sampled/SampledSounds.java
+++ trunk/globalmentor-sound/src/main/java/com/globalmentor/sound/sampled/SampledSounds.java
@@ -22,104 +22,77 @@ import javax.sound.sampled.*;
 
 import com.globalmentor.log.Log;
 
-/**A collection of methods for manipulating sampled sounds.
-@author Garret Wilson
-*/
-public class SampledSounds
-{
+/**
+ * A collection of methods for manipulating sampled sounds.
+ * @author Garret Wilson
+ */
+public class SampledSounds {
 
-	/**Constructs and returns a data line of the specified type from the given
-		input stream. If a <code>Clip</code> is returned it will already be open.
-		The input stream should be one that allows marking so that
-		<code>AudioSystem.getAudioFileFormat()</code> can correctly extract
-		information before the audio is actually read.
-	@param inputStream The input stream of the audio.
-	@param lineClass The type of line desired (for example <code>Clip.class</code>).
-	@return A data line from the input stream. This line will already be open if
-		the line is a <code>Clip</code>.
-	@throws IOException Thrown if there is an error reading from the given
-		input stream.
-	@throws UnsupportedAudioFileException Thrown if the audio file represented
-		by the input stream is not supported.
-	@throws LineUnavailableException Thrown if the line is not available because,
-		for example, it is already in use by another application.
-	*/
-	public static DataLine getDataLine(final InputStream inputStream, final Class<?> lineClass) throws IOException, UnsupportedAudioFileException, LineUnavailableException
-	{
-		AudioInputStream audioInputStream=AudioSystem.getAudioInputStream(inputStream);	//get an audio input stream from the input stream
-		AudioFormat audioFormat=audioInputStream.getFormat(); //get the format of the input stream
-		DataLine.Info dataLineInfo=new DataLine.Info(lineClass, audioFormat);	//create info for a new data line based on the format
-		if(!AudioSystem.isLineSupported(dataLineInfo))  //if we don't support this type of line, try to convert the audio input stream to something we do support
-		{
+	/**
+	 * Constructs and returns a data line of the specified type from the given input stream. If a <code>Clip</code> is returned it will already be open. The input
+	 * stream should be one that allows marking so that <code>AudioSystem.getAudioFileFormat()</code> can correctly extract information before the audio is
+	 * actually read.
+	 * @param inputStream The input stream of the audio.
+	 * @param lineClass The type of line desired (for example <code>Clip.class</code>).
+	 * @return A data line from the input stream. This line will already be open if the line is a <code>Clip</code>.
+	 * @throws IOException Thrown if there is an error reading from the given input stream.
+	 * @throws UnsupportedAudioFileException Thrown if the audio file represented by the input stream is not supported.
+	 * @throws LineUnavailableException Thrown if the line is not available because, for example, it is already in use by another application.
+	 */
+	public static DataLine getDataLine(final InputStream inputStream, final Class<?> lineClass) throws IOException, UnsupportedAudioFileException,
+			LineUnavailableException {
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream); //get an audio input stream from the input stream
+		AudioFormat audioFormat = audioInputStream.getFormat(); //get the format of the input stream
+		DataLine.Info dataLineInfo = new DataLine.Info(lineClass, audioFormat); //create info for a new data line based on the format
+		if(!AudioSystem.isLineSupported(dataLineInfo)) { //if we don't support this type of line, try to convert the audio input stream to something we do support
 			//create a new audio format to convert from the unsupported format;
 			//note that some VBR implementations return negative numbers for sample
 			//  frame sizes, so the following will not always work:
 			//new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, audioFormat.getSampleRate(), audioFormat.getSampleSizeInBits()*2, audioFormat.getChannels(), audioFormat.getFrameSize()*2, audioFormat.getFrameRate(), true)
-			audioFormat=new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
-				audioFormat.getSampleRate(),
-				16,
-				audioFormat.getChannels(),
-				audioFormat.getChannels() * 2,
-				audioFormat.getSampleRate(),
-				false);
-				  //create a new audio input stream with the conversion audio format
-			audioInputStream=AudioSystem.getAudioInputStream(audioFormat, audioInputStream);
-			dataLineInfo=new DataLine.Info(lineClass, audioFormat);	//create info for a new data line based on the new format
+			audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, audioFormat.getSampleRate(), 16, audioFormat.getChannels(), audioFormat.getChannels() * 2,
+					audioFormat.getSampleRate(), false);
+			//create a new audio input stream with the conversion audio format
+			audioInputStream = AudioSystem.getAudioInputStream(audioFormat, audioInputStream);
+			dataLineInfo = new DataLine.Info(lineClass, audioFormat); //create info for a new data line based on the new format
 		}
-		final DataLine dataLine=(DataLine)AudioSystem.getLine(dataLineInfo);	//get a new line from the audio system and return it
-		if(dataLine instanceof Clip)	//if this is a clip
-		{
-			((Clip)dataLine).open(audioInputStream);	//open the clip here, since the calling function does not have access to the audio input stream
+		final DataLine dataLine = (DataLine)AudioSystem.getLine(dataLineInfo); //get a new line from the audio system and return it
+		if(dataLine instanceof Clip) { //if this is a clip
+			((Clip)dataLine).open(audioInputStream); //open the clip here, since the calling function does not have access to the audio input stream
 		}
-		return dataLine;	//return the line we opened
+		return dataLine; //return the line we opened
 	}
 
-	protected static void play(final InputStream inputStream) throws IOException, UnsupportedAudioFileException, LineUnavailableException	//TODO unfinished; finish or delete
-	{
-		AudioInputStream audioInputStream=AudioSystem.getAudioInputStream(inputStream);	//get an audio input stream from the input stream
-		AudioFormat audioFormat=audioInputStream.getFormat();
-			//create a new audio format to convert from the compressed audio
-		AudioFormat conversionAudioFormat=new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
-			audioFormat.getSampleRate(),
-			16,
-			audioFormat.getChannels(),
-			audioFormat.getChannels() * 2,
-			audioFormat.getSampleRate(),
-			false);
-		audioInputStream=AudioSystem.getAudioInputStream(conversionAudioFormat, audioInputStream);	//create a new audio input stream with the conversion audio format
-		audioFormat=conversionAudioFormat;	//use the new audio format now
-		int bytesPerFrame=audioFormat.getFrameSize();
+	protected static void play(final InputStream inputStream) throws IOException, UnsupportedAudioFileException, LineUnavailableException { //TODO unfinished; finish or delete
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream); //get an audio input stream from the input stream
+		AudioFormat audioFormat = audioInputStream.getFormat();
+		//create a new audio format to convert from the compressed audio
+		AudioFormat conversionAudioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, audioFormat.getSampleRate(), 16, audioFormat.getChannels(),
+				audioFormat.getChannels() * 2, audioFormat.getSampleRate(), false);
+		audioInputStream = AudioSystem.getAudioInputStream(conversionAudioFormat, audioInputStream); //create a new audio input stream with the conversion audio format
+		audioFormat = conversionAudioFormat; //use the new audio format now
+		int bytesPerFrame = audioFormat.getFrameSize();
 		DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
-		if(AudioSystem.isLineSupported(dataLineInfo))
-		{
-		  try
-			{
-				SourceDataLine line=(SourceDataLine)AudioSystem.getLine(dataLineInfo);
+		if(AudioSystem.isLineSupported(dataLineInfo)) {
+			try {
+				SourceDataLine line = (SourceDataLine)AudioSystem.getLine(dataLineInfo);
 				line.open(audioFormat);
 				line.start();
-				final int	EXTERNAL_BUFFER_SIZE = 128000;
-				byte[]	abData = new byte[EXTERNAL_BUFFER_SIZE];
-				int	nBytesRead = 0;
-				while (nBytesRead != -1)
-				{
-					try
-					{
+				final int EXTERNAL_BUFFER_SIZE = 128000;
+				byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
+				int nBytesRead = 0;
+				while(nBytesRead != -1) {
+					try {
 						nBytesRead = audioInputStream.read(abData, 0, abData.length);
+					} catch(IOException e) {
+						Log.error(e); //TODO fix
 					}
-					catch (IOException e)
-					{
-						Log.error(e);	//TODO fix
-					}
-					if (nBytesRead >= 0)
-					{
-						int	nBytesWritten = line.write(abData, 0, nBytesRead);
+					if(nBytesRead >= 0) {
+						int nBytesWritten = line.write(abData, 0, nBytesRead);
 					}
 				}
-		  }
-			catch (LineUnavailableException ex)
-			{
-				Log.error(ex);	//TODO fix
-		  }
+			} catch(LineUnavailableException ex) {
+				Log.error(ex); //TODO fix
+			}
 		}
 	}
 }
